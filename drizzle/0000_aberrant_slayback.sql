b226cd7afea4cab65af268ed4e0a6485da7ddf9c
kailash phukan <kailashphukan34@gmail.com>
2026-07-21 12:33:01 +0530

    sql script fixed inside drizzle

--- drizzle/0000_aberrant_slayback.sql
+++ drizzle/0000_aberrant_slayback.sql
@@ -1,17 +1,17 @@
-CREATE TABLE `tasks` (
+CREATE TABLE IF NOT EXISTS `tasks` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`title` text NOT NULL,
 	`icon` text NOT NULL,
 	`theme` text NOT NULL,
 	`parent_id` integer,
-	`is_pinned` integer DEFAULT false NOT NULL,
+	`is_pinned` integer DEFAULT 0 NOT NULL,
 	`daily_goal_seconds` integer,
 	`created_at` integer NOT NULL,
 	`updated_at` integer NOT NULL,
 	FOREIGN KEY (`parent_id`) REFERENCES `tasks`(`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
-CREATE TABLE `time_entries` (
+CREATE TABLE IF NOT EXISTS `time_entries` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`task_id` integer NOT NULL,
 	`date_string` text NOT NULL,
@@ -22,4 +22,4 @@ CREATE TABLE `time_entries` (
 	FOREIGN KEY (`task_id`) REFERENCES `tasks`(`id`) ON UPDATE no action ON DELETE cascade
 );
 --> statement-breakpoint
-CREATE INDEX `task_date_idx` ON `time_entries` (`task_id`,`date_string`);
+CREATE INDEX IF NOT EXISTS `task_date_idx` ON `time_entries` (`task_id`,`date_string`);
